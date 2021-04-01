4a6ff8da43e28057ac2f57bb8e5125a52ecaf7f7
Akhil Chanda <52042860+akhilc668@users.noreply.github.com>
2021-04-01 11:34:13 +0530

    commit

---
+++ dropwizardJDBI3/src/main/java/com/dropwizardJDBI3/health/DatabaseHealthCheck.java
@@ -0,0 +1,24 @@
+package com.dropwizardJDBI3.health;
+
+import com.codahale.metrics.health.HealthCheck;
+
+import io.dropwizard.db.DataSourceFactory;
+
+public class DatabaseHealthCheck extends HealthCheck {
+
+	private final DataSourceFactory database;
+
+    public DatabaseHealthCheck(DataSourceFactory database) {
+        this.database = database;
+    }
+
+    @Override
+    protected Result check() throws Exception {
+        if (database.getCheckConnectionOnConnect()) {
+            return Result.healthy("connected to"+database.getUrl());
+        } else {
+            return Result.unhealthy("Cannot connect to " + database.getUrl());
+        }
+    }
+
+}
